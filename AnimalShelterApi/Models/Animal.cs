852889bcb81d4ac43126aa01c7bb2c8c249e20d7
Patrick Dolan <dolanp1992@gmail.com>
2022-04-22 11:17:57 -0700

    add validations for animal model

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(25)]
     public string Name { get; set; }
+    [Required]
     public string PetType { get; set; }
+    [Required]
     public string Breed { get; set; }
+    [Required]
+    [Range(0, 25, ErrorMessage = "Age must be between 0 and 25.")]
     public int Age { get; set; }
+    [Required]
     public string Gender { get; set; }
     public string Color { get; set; }
   }
