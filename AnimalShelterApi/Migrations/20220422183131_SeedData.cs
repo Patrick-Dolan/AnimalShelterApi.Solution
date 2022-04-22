b18104135b10b80702f927bcb556d3df9f709ef9
Patrick Dolan <dolanp1992@gmail.com>
2022-04-22 11:33:06 -0700

    add seed data

---
+++ AnimalShelterApi/Migrations/20220422183131_SeedData.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PetType",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Animals",
+                type: "varchar(25) CHARACTER SET utf8mb4",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Breed",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Color", "Gender", "Name", "PetType" },
+                values: new object[,]
+                {
+                    { 1, 3, "Persian", "White", "Female", "Fluffy", "Cat" },
+                    { 2, 5, "Persian", "White", "Male", "Skittles", "Cat" },
+                    { 3, 9, "Scotish Fold", "Grey", "Male", "Burger", "Cat" },
+                    { 4, 8, "Husky", "Black and White", "Male", "Loki", "Dog" },
+                    { 5, 12, "Husky", "Black and White", "Female", "Naiya", "Dog" },
+                    { 6, 2, "Husky", "Red", "Male", "Copper", "Dog" },
+                    { 7, 1, "Corgi", "Brown and White", "Male", "Peanut Butter", "Dog" },
+                    { 8, 2, "Calico", "Brown and White", "Female", "Cally", "Cat" },
+                    { 9, 5, "Pomeranian", "Black", "Female", "Princess", "Dog" },
+                    { 10, 7, "Maine Coon", "Black and Brown", "Male", "Pringles", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PetType",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(25) CHARACTER SET utf8mb4",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Breed",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -7,5 +7,22 @@ namespace AnimalShelterApi.Models
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options) { }
 
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Fluffy", PetType = "Cat", Breed = "Persian", Age = 3, Gender = "Female", Color = "White" },
+          new Animal { AnimalId = 2, Name = "Skittles", PetType = "Cat", Breed = "Persian", Age = 5, Gender = "Male", Color = "White" },
+          new Animal { AnimalId = 3, Name = "Burger", PetType = "Cat", Breed = "Scotish Fold", Age = 9, Gender = "Male", Color = "Grey" },
+          new Animal { AnimalId = 4, Name = "Loki", PetType = "Dog", Breed = "Husky", Age = 8, Gender = "Male", Color = "Black and White" },
+          new Animal { AnimalId = 5, Name = "Naiya", PetType = "Dog", Breed = "Husky", Age = 12, Gender = "Female", Color = "Black and White" },
+          new Animal { AnimalId = 6, Name = "Copper", PetType = "Dog", Breed = "Husky", Age = 2, Gender = "Male", Color = "Red" },
+          new Animal { AnimalId = 7, Name = "Peanut Butter", PetType = "Dog", Breed = "Corgi", Age = 1, Gender = "Male", Color = "Brown and White" },
+          new Animal { AnimalId = 8, Name = "Cally", PetType = "Cat", Breed = "Calico", Age = 2, Gender = "Female", Color = "Brown and White" },
+          new Animal { AnimalId = 9, Name = "Princess", PetType = "Dog", Breed = "Pomeranian", Age = 5, Gender = "Female", Color = "Black" },
+          new Animal { AnimalId = 10, Name = "Pringles", PetType = "Cat", Breed = "Maine Coon", Age = 7, Gender = "Male", Color = "Black and Brown" }
+        );
+    }
   }
 }
