e771e3daf073e0ebb2b9902ab6bd17eadd60d3ed
Patrick Dolan <dolanp1992@gmail.com>
2022-04-22 09:27:53 -0700

    add animal shelter context

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterApi.Models
+{
+  public class AnimalShelterApiContext : DbContext
+  {
+    public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options) { }
+
+    public DbSet<Animal> Animals { get; set; }
+  }
+}
