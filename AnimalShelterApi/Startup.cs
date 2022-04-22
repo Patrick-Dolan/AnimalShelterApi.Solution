2b20cc3857bd8f519ef7f527f65d7b8cb0f8323e
Patrick Dolan <dolanp1992@gmail.com>
2022-04-22 09:33:21 -0700

    add dbcontext config to startup

--- AnimalShelterApi/Startup.cs
+++ AnimalShelterApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AnimalShelterApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace AnimalShelterApi
@@ -26,7 +21,8 @@ namespace AnimalShelterApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AnimalShelterApiContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnections"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
